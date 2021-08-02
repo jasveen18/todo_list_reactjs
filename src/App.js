5239d1669a12aa1bb85f7af4b6bf3881d25e70b6
Jasveen Singh Kohli <71898741+jasveen18@users.noreply.github.com>
2021-08-03 02:48:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Resturant from './components/basics/Resturant';
+import Todo from './components/todolist/Todo';
+
+const App = () => {
+  return (
+    <Todo/>
+  )
+}
+
+export default App
